705fd6393595fc2708eee437affd6306edabc27b
Satvik Sharma <sharmasatvik43@gmail.com>
2026-03-25 12:59:49 +0530

    learnt about sliding window

---
+++ src/concept_learnt/E_SlidingWindow.java
@@ -0,0 +1,30 @@
+package concept_learnt;
+
+// Learnt from Longest Substring Without Repeating Characters during last min interview prep, practicing here
+
+import java.util.*;
+
+/**
+ * <h2>Sliding Window Template</h2>
+* <pre>
+ *     int left = 0;
+ *
+ * for (int right = 0; right < n; right++) {
+ *
+ *     // new element include karo
+ *
+ *     while (window galat ho gaya) {
+ *         // window ko chhota karo
+ *         left++;
+ *     }
+ *
+ *     // answer update karo
+ * }
+* </pre>
+* */
+
+public class E_SlidingWindow {
+    public static void main(String[] args) {
+        System.out.println("Read docs");
+    }
+}
