9e7d9b96a0eef376e393bc85cb141f307ea8f572
Satvik Sharma <sharmasatvik43@gmail.com>
2026-03-26 17:45:26 +0530

    Maximum Subarray by Kadane's Algo is done

---
+++ src/M_MaximumSubarray.java
@@ -0,0 +1,20 @@
+// Day 5- 26th of March 2026 - Prob #11
+
+
+public class M_MaximumSubarray {
+    public static void main(String[] args) {
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println("Result: " + maxSubArray(arr));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int current = 0, maximum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            current = Math.max(num, current + num);
+            maximum = Math.max(current, maximum);
+        }
+
+        return maximum;
+    }
+}
