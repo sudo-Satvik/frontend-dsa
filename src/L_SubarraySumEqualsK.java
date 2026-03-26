d4b9c8cd875000a0f5e454af351db5a88e76e60d
Satvik Sharma <sharmasatvik43@gmail.com>
2026-03-26 16:59:31 +0530

    Subarray sum equals done

---
+++ src/L_SubarraySumEqualsK.java
@@ -0,0 +1,26 @@
+// Day 5- 24th of March 2026 - Prob #11
+
+import java.util.HashMap;
+
+public class L_SubarraySumEqualsK {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        System.out.println("Result: " + subarraySum(arr, 3));
+    }
+
+    public static int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0, sum = 0;
+        map.put(0, 1);
+        for (int num : nums) {
+            // storing prefix sum
+            sum += num;
+            // previous = curr - k
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
