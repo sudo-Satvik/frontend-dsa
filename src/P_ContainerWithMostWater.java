a07408cf90cc9e4606e2d1204711d990e8c8c561
Satvik Sharma <sharmasatvik43@gmail.com>
2026-04-04 15:16:51 +0530

    container with most water

---
+++ src/P_ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+// Day 9 - 2nd of April 2026 - Prob #14
+
+public class P_ContainerWithMostWater {
+    public static void main(String[] args) {
+        int[] arr = {1,8,6,2,5,4,8,3,7};
+
+        System.out.println("Result: " + maxArea(arr));
+    }
+
+    public static int maxArea(int[] height) {
+        int start = 0, end = height.length - 1;
+        int max = 0;
+        while (start < end) {
+            int currArea = Math.min(height[start], height[end]) * (end - start);
+            max = Math.max(currArea, max);
+
+            if (height[start] < height[end]) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        return max;
+    }
+}
